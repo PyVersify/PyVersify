bf9c252848b1c0c0da01c85bc899cb5e10bb9266
Nathan-Bransby-NMT <brnsb.dev@gmail.com>
2025-02-03 16:45:56 +0800

    Update workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PyVersify/',
+  base: '/PyVersify',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
